7758a7d30ffc6cf220a699a3ce755eef473a24b6
Oliver Krumbæk Perving <Ollikp@gmail.com>
2023-11-29 14:46:41 +0100

    Mine beskeder styling. Oprettede chat page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,11 @@ const routes: Routes = [
   {
     path: 'tab4',
     loadChildren: () => import('./tab4/tab4.module').then( m => m.Tab4PageModule)
+  },  {
+    path: 'chat',
+    loadChildren: () => import('./pages/chat/chat.module').then( m => m.ChatPageModule)
   }
+
 ];
 @NgModule({
   imports: [
