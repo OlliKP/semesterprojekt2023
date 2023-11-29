1c171fc69ec87fc13f0bc77c7d4383e80173ec02
SofNyb <sofi13a5@edu.zealand.dk>
2023-11-29 10:40:09 +0100

    Oprettet pages-mappe + login-side

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,11 @@ const routes: Routes = [
   {
     path: 'tab4',
     loadChildren: () => import('./tab4/tab4.module').then( m => m.Tab4PageModule)
+  },  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   }
+
 ];
 @NgModule({
   imports: [
