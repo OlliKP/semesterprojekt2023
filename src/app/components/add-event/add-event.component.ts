a6b061796a1ac342b4c62659aeadb880024c2777
Ditte Lykke Hansen <dittehansen143@gmail.com>
2023-11-29 19:27:17 +0100

    Wip

---
+++ src/app/components/add-event/add-event.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-event',
+  templateUrl: './add-event.component.html',
+  styleUrls: ['./add-event.component.scss'],
+})
+export class AddEventComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +9,64 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  // @ViewChild(IonModal) modal: IonModal;
 
-  constructor() {}
+  alertButtons = [
+    {
+      text: 'Nej',
+      handler: () => {
+        console.log('nej');
+      },
+    },
+    {
+      text: 'Ja',
+      // handler: (id) => {
+      //   this.deleteEvent(id);
+      // },
+    },
+  ];
+
+  constructor(private alertController: AlertController) {}
+
+  async deleteAlert() {
+    const alert = await this.alertController.create({
+      header: 'Vil du slette "' + + '"?',
+      buttons: [
+        {
+          text: 'Nej',
+          handler: () => {
+            console.log('nej');
+          },
+        },
+        {
+          text: 'Ja',
+          cssClass: 'alert-button-cancel',
+          handler: () => {
+            this.deleteEvent(event);
+          },
+        },
+      ],
+    });
+    await alert.present();
+  }
+
+  deleteEvent(event: any): void {
+    // this.eventsService.delete(event.id).subscribe(
+    //   (data) => {
+    //     this.retrieveEvents();
+    //   },
+    //   (error) => {
+    //     console.log(error);
+    //   }
+    // );
+  }
+
+  // cancel() {
+  //   this.modal.dismiss(null, 'cancel');
+  // }
+  
+  // save() {
+  //   this.modal.dismiss('confirm');
+  // }
 
 }
