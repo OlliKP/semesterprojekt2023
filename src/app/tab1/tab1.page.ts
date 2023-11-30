fdaa838c7d1b4aef79af1200ed5a48ebf0961a65
Oliver Krumbæk Perving <Ollikp@gmail.com>
2023-11-30 17:10:10 +0100

    Forside firebase CRUD Implementering

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FirebaseService } from '../services/firebase.service';
+
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  events: any;
 
-  constructor() {}
+  constructor(
+    private firebaseService: FirebaseService
+  ) {}
+
+  ngOnInit() {
+    this.fetchEvents();
+  }
+
+  fetchEvents() {
+    this.firebaseService.read_events().subscribe((data) => {
+      this.events = data.map((e) => {
+        return {
+          eventId: e.payload.doc.id,
+          title: e.payload.doc.data()['title'],
+          date: e.payload.doc.data()['date'],
+          description: e.payload.doc.data()['description'],
+          location: e.payload.doc.data()['location'],
+          category: e.payload.doc.data()['category'],
+          minPersons: e.payload.doc.data()['minPersons'],
+          maxPersons: e.payload.doc.data()['maxPersons'],
+        };
+      });
+    });
+  }
+
+  displayNumberOfPerson(event) {
+    if (event.minPersons === event.maxPersons) {
+      return event.minPersons;
+    }
+    return event.minPersons + ' - ' + event.maxPersons;
+  }
 
 }
