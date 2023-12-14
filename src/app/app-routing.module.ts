a44fa88e64fdb173a83774008b7834afdc7c956e
Ditte Lykke Hansen <dittehansen143@gmail.com>
2023-12-14 22:25:19 +0100

    prod setup...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,43 +4,64 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () =>
+      import('./tabs/tabs.module').then((m) => m.TabsPageModule),
+  },
+  {
+    path: 'tabs',
+    loadChildren: () =>
+      import('./tabs/tabs.module').then((m) => m.TabsPageModule),
   },
   {
     path: 'add-event',
-    loadChildren: () => import('./components/add-event/add-event.component').then( m => m.AddEventComponent)
+    loadChildren: () =>
+      import('./components/add-event/add-event.component').then(
+        (m) => m.AddEventComponent
+      ),
   },
   {
     path: 'edit-event',
-    loadChildren: () => import('./components/edit-event/edit-event.component').then( m => m.EditEventComponent)
+    loadChildren: () =>
+      import('./components/edit-event/edit-event.component').then(
+        (m) => m.EditEventComponent
+      ),
   },
   {
     path: 'introduction',
-    loadChildren: () => import('./pages/introduction/introduction.module').then( m => m.IntroductionPageModule)
+    loadChildren: () =>
+      import('./pages/introduction/introduction.module').then(
+        (m) => m.IntroductionPageModule
+      ),
   },
   {
     path: 'add-event',
-    loadChildren: () => import('./components/add-event/add-event.component').then( m => m.AddEventComponent)
+    loadChildren: () =>
+      import('./components/add-event/add-event.component').then(
+        (m) => m.AddEventComponent
+      ),
   },
   {
     path: 'edit-event',
-    loadChildren: () => import('./components/edit-event/edit-event.component').then( m => m.EditEventComponent)
-  },  
+    loadChildren: () =>
+      import('./components/edit-event/edit-event.component').then(
+        (m) => m.EditEventComponent
+      ),
+  },
   {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () =>
+      import('./pages/login/login.module').then((m) => m.LoginPageModule),
   },
   {
     path: 'opret',
-    loadChildren: () => import('./pages/opret/opret.module').then( m => m.OpretPageModule)
-  }
-
-
+    loadChildren: () =>
+      import('./pages/opret/opret.module').then((m) => m.OpretPageModule),
+  },
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC57jBoQhsFB0hUIju1KBkwxCI9Ij0_6gY',
+    authDomain: 'buddysdb.firebaseapp.com',
+    databaseURL:
+      'https://buddysdb-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'buddysdb',
+    storageBucket: 'buddysdb.appspot.com',
+    messagingSenderId: '245567520762',
+    appId: '1:245567520762:web:472651cbf564eb93046e43',
+    measurementId: 'G-HBTH0KTS8B',
+  },
 };
